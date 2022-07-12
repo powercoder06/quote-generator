050b11613d8bfa6cb5c4c300549f71df902e66b3
powercoder06 <ayush.kumar2021c@vitstudent.ac.in>
2022-07-12 17:39:46 +0530

    adding loader

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 let apiQuotes = [];
 
 // Show New Quotes
 function newQuotes() {
+    loading();
     //Pick a random quote from apiQuote array
     const quote=apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if the auther field is blank and replace it with 'Unknown'
@@ -26,10 +38,12 @@ function newQuotes() {
     else{
         quoteText.classList.remove('long-quote');
     }
+    complete();
 }
 // Get quotes from API
 
 async function getQuotes() {
+    loading();
     const apiURL='https://type.fit/api/quotes';
     try {
         const response = await fetch(apiURL);
